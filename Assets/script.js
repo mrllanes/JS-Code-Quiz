d1b5d225508c92d69bd926b4c682f7a9c17a215b
Mr. Llanes <llanesm@gmail.com>
2020-12-18 17:21:41 -0600

    JS Array Builds.

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,34 @@
 //JavaScript file for JS Code Quiz
 
-var questionArray = [""]
+var questionArray = ["What is JavaScript?",
+                        "What does DOM stand for?",
+                        "What does 'var' stand for?",
+                        "What is the name of the most popular front-end open source toolkit?",
+                        "Which <tag> is used to add JavaScript to an HTML page?",
+                        "If you created a function named 'addingNumbers()' How would you call it (to run)?",
+                        "Which set of characters are used when creating an array?",
+                        "Which of the following can be used to create a loop?",
+                        "What is the command used to change a string into an integer?",
+                        "Which of the following is NOT a JavaScript data type?",
+                        "Where do we place the “link” to the JavaScript file?",
+                        "What was JavaScript originally named?",
+                        "Who originally created JavaScript?",
+                        "In what year was JavaScript invented?",
+                        "What “tool” is good to use in conjunction with the browser development tools?"]
+
+var answerOptions0 = ["Coffee Instructions", "Programming Language", "Dilithium Factoring", "Cursive"]
+var answerOptions1 = ["Data On Memory", "Display Open Main", "Document Object Model", "Demo On Monitor"]
+var answerOptions2 = ["Various", "Varnish", "Variform", "Variable"]
+var answerOptions3 = ["Shoestrap", "Bootstrap", "Sneakerstrap", "Slidestrap"]
+var answerOptions4 = ["<java>", "<js>", "<javascript>", "<script>"]
+var answerOptions5 = ["call addingNumbers()", "return addingNumbers()", "var addingNumbers()", "addingNumbers()"]
+var answerOptions6 = ["[ ]", "( )", "\“ \“", "{ }"]
+var answerOptions7 = ["for", "loop", "round", "arc"]
+var answerOptions8 = ["changeInt()", "numberInt()", "stringInt()", "parseInt()"]
+var answerOptions9 = ["number", "box", "object", "string"]
+var answerOptions10 = ["The <body> element", "The <head> element", "Either One", "Neither One"]
+var answerOptions11 = ["JavaScript", "LiveScript", "Coffee", "Mocha"]
+var answerOptions12 = ["Bill Gates", "Brendan Eich", "Jim Clark", "Yan Zhu"]
+var answerOptions13 = ["1992", "1995", "2000", "2001"]
+var answerOptions14 = ["alert", "return", "console.log", "execute.now"]
+
